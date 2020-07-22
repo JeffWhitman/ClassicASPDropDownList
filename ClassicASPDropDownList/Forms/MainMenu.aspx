a8354b80d960d83daeb0f31ce5a370eb67235eaf
JeffWhitman <67132067+JeffWhitman@users.noreply.github.com>
2020-07-22 13:08:30 -0500

    First commit

---
+++ ClassicASPDropDownList/Forms/MainMenu.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainMenu.aspx.cs" Inherits="ClassicASPDropDownList.Forms.MainMenu" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Italic="True" ForeColor="#FF3300" Text="Classic ASP.Net Dropdown List Demo"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="btnDisplaySelection" runat="server" OnClick="btnDisplaySelection_Click" Text="Display Selection" Width="244px" />
+            <br />
+            <br />
+            Countries<br />
+            <asp:DropDownList ID="ddlCountries" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Label ID="lblMessage" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ClassicASPDropDownList/Forms/MainMenu.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClassicASPDropDownList.Forms
+{
+    public partial class MainMenu : System.Web.UI.Page
+    {
+        private void LoadDataControls()
+        {
+            using (JeffTestEntities db = new JeffTestEntities())
+            {
+                var countries = from x in db.Countries orderby x.CountryName select new { x.RecId, x.CountryName };
+
+                ddlCountries.DataSource = countries.ToList();
+                ddlCountries.DataValueField = "RecId";
+                ddlCountries.DataTextField = "CountryName";
+                ddlCountries.DataBind();
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(IsPostBack==false)
+            {
+                LoadDataControls();
+            }
+        }
+
+        protected void btnDisplaySelection_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = ddlCountries.SelectedItem.Text;
+        }
+    }
+}
